72bfe533b83be560dae84075dd58d1c3e996fe98
Propuhganduh <JosephLG27@outlook.com>
2017-01-04 19:45:47 -0700

    Create bot.js

---
+++ bot.js
@@ -0,0 +1,130 @@
+var HTTPS = require('https');
+var cool = require('cool-ascii-faces');
+
+var botID = process.env.BOT_ID;
+
+function respond() {
+  var request = JSON.parse(this.req.chunks[0]),
+      botRegex = /^\/cool guy/;  botRegexDL = /^\/DDL/i;botRegexSalt = /^\/salt/;botRegexBan = /^\/ban/
+      botRegexAd=/^\/advance/;botRegexShrug = /^\/shrug/; botRegexSC = /^\/SDL/i;
+      botRegexP = /^\/PDL/i;  botRegexTw = /^\/twitch/i; botRegexSb = /^\/sub/; botRegexLab = /^\/lab/;
+      botRegexJew = /^\/jew/; botRegexRip = /^\/rip/
+  var teamAb = ["NE","NO","ARI","PHI","CLE","TEN","OAK","DAL","IND","SEA","CIN","PIT","JAC"
+                ,"BAL","SD","DEN","MIN","ATL","KC","NYG","GB","DET","HOU","STL","CHI","CAR",
+                "MIA","BUF","SF","WAS","NYJ","TB"]
+  if(request.text && botRegex.test(request.text)) {
+    this.res.writeHead(200);
+    postMessage(cool());
+    this.res.end();
+  } 
+  else if(request.text && botRegexDL.test(request.text)) {
+    this.res.writeHead(200);
+    postMessage("http://daddyleagues.com/maddenmadness/team/"+request.text.substring(5,8)+"/depthchart");
+    this.res.end();
+  } 
+  else if(request.text && botRegexSalt.test(request.text)) {
+    this.res.writeHead(200);
+    postMessage("http://i.imgur.com/q3ohLtD.gif");
+    this.res.end();
+  } 
+  else if(request.text && botRegexAd.test(request.text)) {
+    this.res.writeHead(200);
+    postMessage("http://www.kno.com/images/store/knoadvance/overview.png");
+    this.res.end();
+  }
+  else if(request.text && botRegexBan.test(request.text)) {
+    this.res.writeHead(200);
+    postMessage("http://www.returnofkings.com/wp-content/uploads/2014/03/ban.jpg");
+    this.res.end();
+  } 
+  else if(request.text && botRegexShrug.test(request.text)) {
+    this.res.writeHead(200);
+    postMessage("¯\\\_(ツ)_/¯");
+    this.res.end();
+  } 
+  else if(request.text && botRegexSC.test(request.text)) {
+    this.res.writeHead(200);
+    postMessage("http://daddyleagues.com/maddenmadness/team/"+request.text.substring(5,8)+"/schedule");
+    this.res.end();
+  }
+  else if(request.text && botRegexP.test(request.text)) {
+    this.res.writeHead(200);
+    var req = request.text.substring(5,request.text.length);
+    var rep = req.replace(/ /,"+");
+    postMessage("http://daddyleagues.com/maddenmadness/players?name="+rep+"&position=all&team=all");
+    this.res.end();
+  } 
+
+  else if(request.text && botRegexTw.test(request.text)) {
+    this.res.writeHead(200);
+    postMessage("http://www.twitch.tv/"+request.text.substring(8,request.text.length));
+    this.res.end();
+  } 
+  else if(request.text && botRegexSb.test(request.text)) {
+    this.res.writeHead(200);
+    postMessage("http://i.imgur.com/yNBtNiy.gif");
+    this.res.end();
+  } 
+  else if(request.text && botRegexLab(nah).test(request.text)) {
+    this.res.writeHead(200);
+    postMessage("http://www.cornelllab.com/images/70.jpg");
+    this.res.end();
+  } 
+  else if(request.text && botRegexJew(nah).test(request.text)) {
+    this.res.writeHead(200);
+    postMessage("http://i.groupme.com/307x362.gif.357292f527ad49a38da4e13201fbfa77.large");
+    this.res.end();
+  } 
+   else if(request.text && botRegexRip(nah).test(request.text)) {
+    this.res.writeHead(200);
+    postMessage("http://i1.wp.com/whineylimey.com/wp-content/uploads/2014/03/wordoftheweek.jpg?resize=720%2C320");
+    this.res.end();
+  } 
+  else {
+    console.log("don't care");
+    this.res.writeHead(200);
+    this.res.end();
+  }
+}
+
+function postMessage(response) {
+  var botResponse,options, body, botReq;
+
+  botResponse = response
+
+  options = {
+    hostname: 'api.groupme.com',
+    path: '/v3/bots/post',
+    method: 'POST'
+  };
+
+  body = {
+    "bot_id" : botID,
+    "text" : botResponse
+  };
+
+  console.log('sending ' + botResponse + ' to ' + botID);
+
+  botReq = HTTPS.request(options, function(res) {
+      if(res.statusCode == 202) {
+        //neat
+      } else {
+        console.log('rejecting bad status code ' + res.statusCode);
+      }
+  });
+
+  botReq.on('error', function(err) {
+    console.log('error posting message '  + JSON.stringify(err));
+  });
+  botReq.on('timeout', function(err) {
+    console.log('timeout posting message '  + JSON.stringify(err));
+  });
+  botReq.end(JSON.stringify(body));
+}
+
+function getRandomInt(min, max) {
+  return Math.floor(Math.random() * (max - min)) + min;
+}
+
+
+exports.respond = respond;
